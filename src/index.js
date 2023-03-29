a56798d7db811c0c40448b2e783343d3d8833191
Fransua Boltof <bolt2000@ukr.net>
2023-03-29 22:06:28 +0300

    лоьтрп

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Notiflix from 'notiflix';
 
-import SimpleLightbox from "simplelightbox";
+// import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm"
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 
