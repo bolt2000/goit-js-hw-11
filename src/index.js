066811b1db87f4e3dc76e5558ba6a4056d6b717f
Fransua Boltof <bolt2000@ukr.net>
2023-03-30 19:55:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 // import axios from 'axios';
-import NewsApiService from "./js/";
+import NewsApiService from "./js/api-service";
 import hitsTpl from "./templates/hits.hbs";
 
 const refs = {
